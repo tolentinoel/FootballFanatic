16e2a5b9ee1e0bef8b239de4db2244ed78188beb
tolentinoel <rckrleyn@gmail.com>
2021-02-04 22:06:48 -0800

    Revert "cleaned up commented out code"
    This reverts commit 6989d54f60276c2c02a05a25dfcf914745e179c3.

--- app/api_communicator.rb
+++ app/api_communicator.rb
@@ -20,7 +20,7 @@ class APICommunicator
             city == stadium["city"]
             puts "========================".colorize(:green)
             puts stadium["name"].colorize(:green)
-        else
+        else 
             puts "========================".colorize(:green)
             puts "No stadiums found in this city.".colorize(:red)
         end
@@ -37,7 +37,7 @@ class APICommunicator
             away_team_name = Team.find_by(id: away_team_id)["name"]
             puts "========================".colorize(:green)
             puts "#{home_team_name} vs. #{away_team_name}".colorize(:green)
-        else
+        else 
             puts "========================".colorize(:green)
             puts "No games found on this date.".colorize(:red)
         end
@@ -61,7 +61,7 @@ class APICommunicator
                 puts "========================".colorize(:green)
                 puts "#{date}".colorize(:green)
                 puts "#{result.name} vs. #{home_team_name}".colorize(:green)
-
+                
                 else
                 home_games_obj = Game.find_by(home_team_id: result.id)
                 away_team_id = home_games_obj["away_team_id"]
@@ -75,7 +75,7 @@ class APICommunicator
                 puts "========================".colorize(:green)
                 puts "#{date}".colorize(:green)
                 puts "#{result.name} vs. #{away_team_name}".colorize(:green)
-            end
+            end 
         else puts "========================".colorize(:red)
             puts "No games found for this team.".colorize(:red)
         end
@@ -92,12 +92,75 @@ class APICommunicator
             puts "========================".colorize(:green)
             puts "#{home_team} vs. #{away_team}".colorize(:green)
             puts "#{the_date}".colorize(:green)
-        else
+        else 
             puts "========================".colorize(:red)
             puts "No games found at this stadium.".colorize(:red)
         end
     end
 
 
+    # def gets_stadiums_by_city(city)
+    #     all_venues = []
+    #     all_games.each do |games|
+    #         games["_embedded"]["venues"].each do |vi|
+    #             city_name = vi["city"]["name"]
+    #             if city == city_name
+    #                 all_venues << vi["name"]
+    #             end
+    #         end
+    #     end
+    #     puts
+    #     puts all_venues.uniq
+    #     # puts "No stadiums found in this city."
+    # end
+
+    # def gets_stadiums_by_city(city)
+    #     Stadium.find_by city: city
+    #     # binding.pry
+    # end
+    # gets_stadiums_by_city("Houston")
+
+    # def gets_games_by_date(date)
+    #     all_games_on_this_day = []
+    #     all_games.each do |games|
+    #         game_date = games["dates"]["start"]["localDate"]
+    #         if date == game_date
+    #             all_games_on_this_day << games["name"]
+    #         end
+    #     end
+    #     puts
+    #     puts all_games_on_this_day
+    # end
+
+
+    # def gets_schedule_by_team(team)
+    #     team_schedule = []
+    #     all_games.each do |games|
+    #         game_teams = games["name"].split(" vs. ")
+    #         if game_teams.include?(team)
+    #             team_schedule << games["dates"]["start"]["localDate"]
+    #             team_schedule << games["name"]
+    #         end
+    #     end
+    #     puts
+    #     puts team_schedule
+    # end
+    # gets_schedule_by_team("Philadelphia Eagles")
+
+    # def gets_games_by_stadium(stadium)
+    #     all_games_at_stadium = []
+    #     all_games.each do |games|
+    #         games["_embedded"]["venues"].each do |vi|
+    #             stadium_name = vi["name"]
+    #             if stadium == stadium_name
+    #                 all_games_at_stadium << games["dates"]["start"]["localDate"]
+    #                 all_games_at_stadium << games["name"]
+    #             end
+    #         end
+    #     end
+    #     puts
+    #     puts all_games_at_stadium
+    # end
+    # gets_games_by_stadium("Raymond James Stadium")
 
 end
